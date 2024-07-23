1c6b25b00975ce93eb1b1b325373225ffe95059f
Ritikrai09 <ritikrai489@gmail.com>
2024-07-23 17:14:30 +0530

    Addding single Video and youtube support with export url

---
+++ lib/preload_video_player.dart
@@ -0,0 +1,5 @@
+library preload_video_player;
+
+export 'package:cached_video_player_plus/cached_video_player_plus.dart';
+export 'package:flutter_preload_videos/video_apis.dart';
+export 'package:flutter_preload_videos/provider/preload_provider.dart';
